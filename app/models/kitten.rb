6121f380d42ad7937b46609f31ef6685b4088ad9
Julian DeGroot-Lutzner <jdegroot@students.pitzer.edu>
2020-12-09 10:57:45 -0500

    scaffold Kitten model and add validations

---
+++ app/models/kitten.rb
@@ -0,0 +1,6 @@
+class Kitten < ApplicationRecord
+  validates :name, :age, :cuteness, :softness, presence: true
+  validates :name, length: { in: 2..32 }
+  validates :age, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10_000 }
+  validates :cuteness, :softness, numericality: { greater_than_or_equal_to: -1, less_than_or_equal_to: 11 }
+end
